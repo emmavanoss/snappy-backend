6302c3eee7f66a17b1abfd3ac101bb14bd9f36be
Emma Van Oss <e.vanoss@gmail.com>
2020-01-18 17:05:22 +0000

    add CORS headers

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "snappy.pictures");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/hello-world', (req, res) => {
-  res.send('Hello, World')
+  res.send('Hello, World');
 })
 
 app.listen(port, () => {
-  console.log(`Listening at port ${port}`)
+  console.log(`Listening at port ${port}`);
 })
 
