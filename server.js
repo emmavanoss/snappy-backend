8a29cf1f054ea6baf8c931a56570a8d5e408af73
Emma Van Oss <e.vanoss@gmail.com>
2020-01-18 17:11:29 +0000

    updated CORS headers

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "snappy.pictures");
+  res.header("Access-Control-Allow-Origin", "https://snappy.pictures");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
