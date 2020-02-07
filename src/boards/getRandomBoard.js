5773476fdf479502a8973552f01bb1fb5efcedc7
Emma Van Oss <e.vanoss@gmail.com>
2020-02-07 15:10:44 +0000

    remove "solution" property from board object

--- src/boards/getRandomBoard.js
+++ src/boards/getRandomBoard.js
@@ -63,7 +63,6 @@ const getRandomBoard = () => {
     category: category,
     boardName: boardName,
     tiles: makeTileGrid(category, boardName),
-    solution: makeTileGrid(category, boardName)
   }
 }
 
